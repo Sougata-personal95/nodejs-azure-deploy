919ba08c95aa563aae9b9a57f6237040007c4915
Sougata <palsougata217@gmail.com>
2025-03-22 13:39:56 +0530

    fix deployment issue

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-abc;
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Sample Route
+app.get("/", (req, res) => {
+  res.send("Welcome to the Node.js Server!");
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
